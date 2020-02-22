d082ce93c69f0adfa994456e83356d2150c3868b
ifontji <ifontji@gmail.com>
2020-02-22 19:00:44 +0100

    El quinto

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Hello World!\n");
 console.log("Hello World2!\n");
 console.log("Hello World3!\n");
-console.log("Hello World4!\n");
+console.log("Hello World4!\n");Hola que parseFloat.apply.apply. esto no compila.
